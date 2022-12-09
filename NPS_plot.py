8388cce58a3108dfbcea581f1b79d40102ff4eae
Steffensen <josste@ihelse.net>
2022-12-09 15:43:16 +0100

    I have started the procedure of calculating NPS. Now, I have averaged the fft of the ROI in a image series, squared and multiplied with a normalization factor containing pixel size and pixels in the ROI. Next step i radial averaging to get a 1D plot.

--- NPS_plot.py
+++ NPS_plot.py
@@ -14,6 +14,7 @@ import glob
 import numpy as np
 import os
 import cv2
+import scipy.fft as fft 
 
 working_directory = 'F:/Røntgen/Arbeidsmappe/2022/2022 Filtermatching_GUI/CT bilder av Catphan/Body' #os.getcwd()
 
@@ -23,8 +24,8 @@ layout = [
     [sg.Text("Import image series:")],
     [sg.InputText(key='-FOLDER_PATH-', enable_events=True),
      sg.FolderBrowse(initial_folder=working_directory)],
-    # View FOV
-    [sg.Button("Check FOV", key='-FOV-', disabled=True)]
+    # View ROI
+    [sg.Button("Check ROI", key='-ROI-', disabled=True)]
 ]
 window = sg.Window("Noise Power Spectrum", layout, size=((450,300)))
 
@@ -44,62 +45,79 @@ def sortImages(pathname):
 class Images:
     def __init__(self, folder_path):
        self.sortDict, self.sortedKeys = sortImages(folder_path) #Sort images
-       self.FOVcorner = 150
-       self.FOVsize = 200
+       self.ROIcorner = 150
+       self.ROIsize = 200
        
-    def showFOV(self):
-        # Using one of images to display FOV
+    def showROI(self):
+        # Using one of images to display ROI
         data0 = self.sortDict[self.sortedKeys[0]]
-        dicom0 = pydicom.dcmread(data0)
-        image0 = dicom0.pixel_array
-        image0 = image0 * dicom0.RescaleSlope + dicom0.RescaleIntercept
+        self.dicom0 = pydicom.dcmread(data0)
+        image0 = self.dicom0.pixel_array
+        image0 = image0 * self.dicom0.RescaleSlope + self.dicom0.RescaleIntercept
         fig, ax = plt.subplots()
         im = ax.imshow(image0, cmap='Greys_r', vmin=-100, vmax=200)
         
-        # Marking FOV
-        rect = mpl.patches.Rectangle((self.FOVcorner, self.FOVcorner), self.FOVsize, self.FOVsize, linewidth=1, edgecolor='r', facecolor='none')
+        # Marking ROI
+        rect = mpl.patches.Rectangle((self.ROIcorner, self.ROIcorner), self.ROIsize, self.ROIsize, linewidth=1, edgecolor='r', facecolor='none')
         ax.add_patch(rect)
         plt.show()
         
         
         #fig, ax = plt.subplots()
-        #im = ax.imshow(image0[self.FOVcorner:self.FOVcorner+self.FOVsize, self.FOVcorner:self.FOVcorner+self.FOVsize])
+        #im = ax.imshow(image0[self.ROIcorner:self.ROIcorner+self.ROIsize, self.ROIcorner:self.ROIcorner+self.ROIsize])
         #plt.show()
     
-    def averageFOV(self):
-        # Summing up FOV from images series and average
-        averageFOV = np.zeros((self.FOVsize, self.FOVsize))
+    def averageROI(self):
+        # Summing up ROI from images series and average
+        averageROI = np.zeros((self.ROIsize, self.ROIsize))
         self.N = len(self.sortedKeys)
-        # FOV cube to contain the whole FOV.
-        self.FOV_cube = np.empty((self.FOVsize, self.FOVsize, self.N))
+        # ROI cube to contain the whole ROI.
+        self.ROI_cube = np.empty((self.ROIsize, self.ROIsize, self.N))
         i = 0
         plt.figure()
         for key in self.sortedKeys:
-            # Average FOV
+            # Average ROI
             data = self.sortDict[key]
             dicom = pydicom.dcmread(data)
             image = dicom.pixel_array
             image = image * dicom.RescaleSlope + dicom.RescaleIntercept
-            FOV = image[self.FOVcorner:self.FOVcorner+self.FOVsize, self.FOVcorner:self.FOVcorner+self.FOVsize]
-            averageFOV += FOV
+            ROI = image[self.ROIcorner:self.ROIcorner+self.ROIsize, self.ROIcorner:self.ROIcorner+self.ROIsize]
+            averageROI += ROI
             
-            # Making FOV cube
-            self.FOV_cube[:,:,i] = FOV
+            # Making ROI cube
+            self.ROI_cube[:,:,i] = ROI
             i+=1
         print(f'N: {self.N}')
-        self.averageFOV = averageFOV/self.N
-        plt.imshow(self.averageFOV, cmap='Greys_r') 
+        self.averageROI = averageROI/self.N
+        plt.imshow(self.averageROI, cmap='Greys_r') 
     
     def singleNPS(self):
-        FOV_sub = self.FOV_cube[:,:,0] - self.averageFOV
-        plt.figure()
-        plt.imshow(FOV_sub, cmap='Greys_r')
+        # Get the pixel size of the image series
+        px_sz = self.dicom0[0x28, 0x30]
+        px_sz_row = px_sz[0] # pixel row spacing in mm
+        px_sz_col = px_sz[1] # pixel column spacing in mm
         
-        g = self.FOV_cube[:,0,0]
-        g_bar = self.averageFOV[:,0]
-        g_sub = g - g_bar
+        # frame for summing fft images
+        self.averageROI_fft = np.zeros((self.ROIsize, self.ROIsize))
+        # summing fft of ROIs
+        for i in range(self.N):
+            ROI_sub = self.ROI_cube[:,:,i] - self.averageROI
+            ROI_fft = fft.fft2(ROI_sub)
+            ROI_fft_mod2 = np.real(ROI_fft)**2 + np.imag(ROI_fft)**2*(px_sz_row*px_sz_col)/(self.ROIsize**2)
+            self.averageROI_fft += ROI_fft_mod2
+        self.averageROI_fft = self.averageROI_fft/self.N
+        plt.figure()
+        plt.imshow(self.averageROI_fft, cmap='Greys_r', vmin=min(self.averageROI_fft[50,:]), vmax=max(self.averageROI_fft[50,:]))
+        plt.show()
+        
+        
         
         '''
+        g = self.ROI_cube[:,0,0]
+        g_bar = self.averageROI[:,0]
+        g_sub = g - g_bar
+        
+        
         plt.figure()
         plt.plot(g, label='g')
         plt.plot(g_bar, label='g_bar')
@@ -107,8 +125,8 @@ class Images:
         plt.legend()
         plt.show()
         '''
-    def setFOV(self):
-        print('FOV')
+    def setROI(self):
+        print('ROI')
     
     def NPS(self):
         print('NPS')
@@ -121,13 +139,13 @@ while True:
     event, values = window.read()
     
     if event == '-FOLDER_PATH-':
-        window['-FOV-'].update(disabled=False)
+        window['-ROI-'].update(disabled=False)
         
-    if event == '-FOV-':
+    if event == '-ROI-':
         path = values['-FOLDER_PATH-'] + '/*'
         images = Images(path)
-        images.showFOV()
-        images.averageFOV()
+        images.showROI()
+        images.averageROI()
         images.singleNPS()
         
         

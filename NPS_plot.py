e403724a7a41a58e90a2e8ff2ea8cceb70e99c84
Steffensen <josste@ihelse.net>
2022-12-12 14:13:02 +0100

    Had to use fftshift to gather 0 frequencies in the center of the image. Radial averaging done. Still need labels on NPS plot and need to figure out why there is a spike at low frequencies. Later, I want to add the possibility to scroll through images and select which ones to inculde in analysis.

--- NPS_plot.py
+++ NPS_plot.py
@@ -13,7 +13,6 @@ import pydicom
 import glob
 import numpy as np
 import os
-import cv2
 import scipy.fft as fft 
 
 working_directory = 'F:/Røntgen/Arbeidsmappe/2022/2022 Filtermatching_GUI/CT bilder av Catphan/Body' #os.getcwd()
@@ -25,7 +24,9 @@ layout = [
     [sg.InputText(key='-FOLDER_PATH-', enable_events=True),
      sg.FolderBrowse(initial_folder=working_directory)],
     # View ROI
-    [sg.Button("Check ROI", key='-ROI-', disabled=True)]
+    [sg.Button("Check ROI", key='-ROI-', disabled=True)],
+    [sg.Button("2D NPS", key='-2DNPS-', disabled=True)],
+    [sg.Button("NPS", key='-NPS-', disabled=True)]
 ]
 window = sg.Window("Noise Power Spectrum", layout, size=((450,300)))
 
@@ -46,12 +47,13 @@ class Images:
     def __init__(self, folder_path):
        self.sortDict, self.sortedKeys = sortImages(folder_path) #Sort images
        self.ROIcorner = 150
-       self.ROIsize = 200
+       self.ROIsize = 201
+       # Using one of images to display ROI
+       data0 = self.sortDict[self.sortedKeys[0]]
+       self.dicom0 = pydicom.dcmread(data0)
        
     def showROI(self):
-        # Using one of images to display ROI
-        data0 = self.sortDict[self.sortedKeys[0]]
-        self.dicom0 = pydicom.dcmread(data0)
+        
         image0 = self.dicom0.pixel_array
         image0 = image0 * self.dicom0.RescaleSlope + self.dicom0.RescaleIntercept
         fig, ax = plt.subplots()
@@ -62,7 +64,7 @@ class Images:
         ax.add_patch(rect)
         plt.show()
         
-        
+        # Code to only display ROI-image.
         #fig, ax = plt.subplots()
         #im = ax.imshow(image0[self.ROIcorner:self.ROIcorner+self.ROIsize, self.ROIcorner:self.ROIcorner+self.ROIsize])
         #plt.show()
@@ -74,7 +76,6 @@ class Images:
         # ROI cube to contain the whole ROI.
         self.ROI_cube = np.empty((self.ROIsize, self.ROIsize, self.N))
         i = 0
-        plt.figure()
         for key in self.sortedKeys:
             # Average ROI
             data = self.sortDict[key]
@@ -89,9 +90,10 @@ class Images:
             i+=1
         print(f'N: {self.N}')
         self.averageROI = averageROI/self.N
-        plt.imshow(self.averageROI, cmap='Greys_r') 
+        # plt.figure()
+        # plt.imshow(self.averageROI, cmap='Greys_r') 
     
-    def singleNPS(self):
+    def fft2_avg(self):
         # Get the pixel size of the image series
         px_sz = self.dicom0[0x28, 0x30]
         px_sz_row = px_sz[0] # pixel row spacing in mm
@@ -105,14 +107,44 @@ class Images:
             ROI_fft = fft.fft2(ROI_sub)
             ROI_fft_mod2 = np.real(ROI_fft)**2 + np.imag(ROI_fft)**2*(px_sz_row*px_sz_col)/(self.ROIsize**2)
             self.averageROI_fft += ROI_fft_mod2
-        self.averageROI_fft = self.averageROI_fft/self.N
+        self.averageROI_fft = fft.fftshift(self.averageROI_fft/self.N)
+        
+    def NPS_2D(self):
         plt.figure()
         plt.imshow(self.averageROI_fft, cmap='Greys_r', vmin=min(self.averageROI_fft[50,:]), vmax=max(self.averageROI_fft[50,:]))
         plt.show()
         
+    def radial_avg(self):
+        # Creds to Naveen Venkatesan for the idea for this function
+        cen_x = 100
+        cen_y = 100
         
+        # Find radial distances 
+        [X, Y] = np.meshgrid(np.arange(self.ROIsize)-cen_x, np.arange(self.ROIsize)-cen_y)
+        R = np.sqrt(np.square(X)+np.square(Y))
+        
+        rad = np.arange(1, np.max(R), 1)
+        intensity = np.zeros(len(rad))
+        index = 0
+        bin_size = 1
+        
+        for i in rad:
+            mask = (np.greater(R, i - bin_size) & np.less(R, i + bin_size))
+            rad_values = self.averageROI_fft[mask]
+            intensity[index] = np.mean(rad_values)
+            index += 1
+            
+        # Create figure and add subplot
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+        # Plot data
+        ax.plot(rad, intensity, linewidth=2)
+        # Edit axis labels
+        ax.set_xlabel('Radial Distance', labelpad=10)
+        ax.set_ylabel('Average Intensity', labelpad=10)
         
         '''
+        # Code to plot a 1D-line in the image and disply signal - average.
         g = self.ROI_cube[:,0,0]
         g_bar = self.averageROI[:,0]
         g_sub = g - g_bar
@@ -140,14 +172,25 @@ while True:
     
     if event == '-FOLDER_PATH-':
         window['-ROI-'].update(disabled=False)
+        window['-2DNPS-'].update(disabled=False)
+        window['-NPS-'].update(disabled=False)
         
-    if event == '-ROI-':
+        # Make an Images class object
         path = values['-FOLDER_PATH-'] + '/*'
         images = Images(path)
-        images.showROI()
-        images.averageROI()
-        images.singleNPS()
         
+        # NPS calculations
+        images.averageROI()
+        images.fft2_avg()
+        
+    if event == '-ROI-':
+        images.showROI()
+        
+    if event == '-2DNPS-':
+        images.NPS_2D()
+        
+    if event == '-NPS-':
+        images.radial_avg()
         
     
     if event == sg.WIN_CLOSED:
